8a6a8c55448c98614f9d1f70e6bd805e19de0a67
Pavel Klevtsov <klevtsov.p@gmail.com>
2026-03-21 07:46:58 +0300

    demo init

--- airflow/dags/sql/create_mart_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS mart;
-
-CREATE TABLE IF NOT EXISTS mart.client_spend_report (
-    client_name VARCHAR(100),
-    total_spent DECIMAL(12, 2),
-    generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
